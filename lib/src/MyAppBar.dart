8e86623a13c0f9d6e6aadba657c6f716a8a8ce4e
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-23 19:21:06 +0700

    Create(Component): MyAppBar.dart

---
+++ lib/src/MyAppBar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget {
+  const MyAppBar({required this.title, super.key});
+
+  // Fields in a Widget subclass are always marked "final".
+
+  final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 56, // in logical pixels
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      decoration: BoxDecoration(color: Colors.blue[500]),
+      // Row is a horizontal, linear layout.
+      child: Row(
+        children: [
+          const IconButton(
+              icon: Icon(Icons.menu),
+              tooltip: 'Navigation menu',
+              onPressed: null,
+          ),
+          // Expanded expands its child
+          // to fill the available space.
+          Expanded(
+            child: title,
+          ),
+          const IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
+          )
+        ],
+      ),
+    );
+  }
+}
