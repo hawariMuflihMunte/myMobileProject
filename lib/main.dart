a150bfe9c4756483ca32653dcc183e3624b695a5
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-23 19:55:20 +0700

    Create(Component): MyButton.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/src/MyButton.dart';
 import 'package:mobile/src/MyScaffold.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       title: 'My App',
-      home: SafeArea(
-        child: MyScaffold(),
+      home: Scaffold(
+        body: Center(
+          child: MyButton(),
+        ),
       ),
     ),
   );
