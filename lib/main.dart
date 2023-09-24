f9e31dbbfdb26c696f2802ef2c9df382bdc2f9a1
hawariMuflihMunte <hawari.muflih.munte@gmail.com>
2023-09-24 21:06:01 +0700

    Upload(Assets): android_logo.png

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:mobile/src/MyButton.dart';
-import 'package:mobile/src/MyScaffold.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      title: 'My App',
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: MyButton(),
+        appBar: AppBar(
+          title: const Text("Image from assets"),
         ),
+        body: const Image(image: AssetImage('assets/images/android_logo.png')),
       ),
     ),
   );
